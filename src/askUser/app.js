0c75552feff7173c667d26ddbabb6c25981f2a4d
Milo <milo0@users.noreply.github.com>
2020-01-06 20:23:25 +0100

    Fix string interpolation for API Gateway endpoints in askUser Lambda

--- src/askUser/app.js
+++ src/askUser/app.js
@@ -4,8 +4,8 @@ var sns = new AWS.SNS();
 
 exports.handler = async (event,context) => {
 
-    var approveLink = `process.env.APIAllowEndpoint?token=${JSON.stringify(event.token)}`
-    var denyLink = `process.env.APIDenyEndpoint?token=${JSON.stringify(event.token)}`
+    var approveLink = `${process.env.APIAllowEndpoint}?token=${JSON.stringify(event.token)}`
+    var denyLink = `${process.env.APIDenyEndpoint}?token=${JSON.stringify(event.token)}`
     var params = {
                     TopicArn: process.env.Topic,
                     Message: `A restricted Policy change has been detected Approve:${approveLink} Or Deny:${denyLink}` 
